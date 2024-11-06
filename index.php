3025bdfbbdf226d599d595cf0865a88157e76e06
Aly <d-aly@outlook.fr>
2024-11-06 18:26:21 +0100

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,12 @@
             <a href="oeuvre.php?id=<?= $oeuvre['id'] ?>">
                 <img src="<?= $oeuvre['image'] ?>" alt="<?= $oeuvre['titre'] ?>">
                 <h2><?= $oeuvre['titre'] ?></h2>
-                <p class="description"><?= $oeuvre['artiste'] ?></p>
+                <p class="description"><?= $oeuvre['artiste'] . "<br>"; ?>                 
+                <?php
+                date_default_timezone_set("Europe/Paris"); 
+                echo date('d/m/Y h:i:sa')  ; ?>
+                </p>
+
             </a>
         </article>
     <?php  } ?>
