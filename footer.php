161b4e9ae7d6ac29e83e02999dcb84ed56f51ade
Aly <d-aly@outlook.fr>
2024-11-06 18:31:46 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,7 +1,11 @@
 </main>
 <footer>
     <p>
-        <strong>© THE ARTBOX par Aly Diabira</strong> - <em>Tous droits réservés</em>
+        <strong>© THE ARTBOX par Aly Diabira 
+            <?php
+                date_default_timezone_set("Europe/Paris"); 
+                echo date('d/m/Y h:i:sa')  ; ?>
+                </strong> - <em>Tous droits réservés</em>
     </p>
 </footer>
 </body>
