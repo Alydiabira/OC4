fe77f5a21ba8c7ef0a4048524141acc5709c3874
Aly <d-aly@outlook.fr>
2024-11-27 12:11:11 +0100

    Update traitement.php

--- traitement.php
+++ traitement.php
@@ -1,5 +1,51 @@
 <?php
 
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Vérifier si le champ titre est rempli
+    if (empty($_POST['titre'])) {
+        echo "Le nom est requis.<br>";
+    } else {
+        $titre = $_POST['titre'];
+    }
+
+    // Vérifier si le champ description est rempli et s'il est valide
+    if (empty($_POST['artiste'])) {
+        echo "La description est requis.<br>";
+    } else {
+        $artiste = $_POST['artiste'];
+        if (!filter_var($description, FILTER_VALIDATE_URL)) {
+            echo "L'artiste n'est pas valide.<br>";
+        }
+    }
+
+    // Vérifier si le champ description est rempli et s'il est valide
+    if (empty($_POST['description'])) {
+        echo "La description est requis.<br>";
+    } else {
+        $description = $_POST['description'];
+        if (!filter_var($description, FILTER_VALIDATE_URL)) {
+            echo "La description n'est pas valide.<br>";
+        }
+    }
+
+     // Vérifier si le champ email est rempli et s'il est valide
+    if (empty($_POST['image'])) {
+        echo "L'image est requis.<br>";
+    } else {
+        $image = $_POST['image'];
+        if (!filter_var($image, FILTER_VALIDATE_URL)) {
+            echo "L'image n'est pas valide.<br>";
+        }
+    }
+
+}
+
+
 
 if(empty($_POST['titre']) 
     || empty($_POST['artiste']) 
