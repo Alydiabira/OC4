7bfa0b20cce8c1852ed3e10c8bb39bd9d37c5936
Aly <d-aly@outlook.fr>
2025-03-10 18:26:35 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,26 @@
 <?php
-    include 'header.php';
-    include 'bdd.php';
-    $bdd = connexion();
-    $oeuvres = $bdd->query('SELECT * FROM oeuvres');
+include 'header.php';
+include 'bdd.php';
+$bdd = connexion();
 
+// Récupération des œuvres sous forme de tableau associatif
+$oeuvres = $bdd->query('SELECT * FROM oeuvres')->fetchAll(PDO::FETCH_ASSOC);
 ?>
+
 <div id="liste-oeuvres">
-    <?php foreach($oeuvres as $oeuvre) {?>
-        <article class="oeuvre">
-            <a href="oeuvre.php?id=<?= $oeuvre['id'] ?>">
-                <img src="<?= $oeuvre['image'] ?>" alt="<?= $oeuvre['titre'] ?>">
-                <h2><?= $oeuvre['titre'] ?></h2>
-                <p class="description"><?= $oeuvre['artiste'] . "<br>"; ?></p>
-            </a>
-        </article>
-    <?php  } ?>
+    <?php if (count($oeuvres) > 0): ?>
+        <?php foreach($oeuvres as $oeuvre): ?>
+            <article class="oeuvre">
+                <a href="oeuvre.php?id=<?= htmlspecialchars($oeuvre['id']) ?>">
+                    <img src="<?= htmlspecialchars($oeuvre['image']) ?>" alt="<?= htmlspecialchars($oeuvre['titre']) ?>">
+                    <h2><?= htmlspecialchars($oeuvre['titre']) ?></h2>
+                    <p class="description"><?= htmlspecialchars($oeuvre['artiste']) ?></p>
+                </a>
+            </article>
+        <?php endforeach; ?>
+    <?php else: ?>
+        <p>Aucune œuvre disponible pour le moment.</p>
+    <?php endif; ?>
 </div>
+
 <?php include 'footer.php'; ?>
