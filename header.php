f838719835a1ab83a3824ccdf65d09f43543b519
Aly <d-aly@outlook.fr>
2025-03-10 18:26:32 +0100

    Update header.php

--- header.php
+++ header.php
@@ -2,20 +2,25 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-        content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/style.css">
     <title>The ArtBox</title>
 </head>
 <body>
+
 <header>
-    <a href="index.php"><img src="img/logo.png" alt="Logo Artbox" id="logo"></a>
-    <nav>
+    <h1>
+        <a href="index.php">
+            <img src="img/logo.png" alt="Logo de The ArtBox, galerie d'art en ligne" id="logo">
+        </a>
+    </h1>
+    <nav aria-label="Navigation principale">
         <ul>
             <li><a href="index.php">Accueil</a></li>
             <li><a href="ajouter.php">Ajouter une œuvre</a></li>
         </ul>
     </nav>
 </header>
+
 <main>
